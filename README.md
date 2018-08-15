f1fa750a8e5b0d31f8ccf301b0d141205bdd68bc
dasMulli <martin.andreas.ullrich@gmail.com>
2018-08-15 04:28:14 +0200

    Add initial version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AssemblyInfoGenerationSdk
-Extracts .NET SDK AssemblyInfo.cs generation into an SDK for use in legacy project types
+
+Generates assembly attributes ("AssemblyInfo.cs") based on MSBuild properties for classic .NET project types (classic ASP.NET, WPF, WinForms, Console, ...) similar to .NET Core and .NET Standard project.
+
+It containes isolated copies of the `Microsoft.NET.Sdk` build logic and uses MSBuild task dependency isolation logic by @AArnott to do this in a compatible and non-conflicting way.
+
+# Usage
+
+Use this in your favourite project by [referencing it as MSBuild Project SDK](https://docs.microsoft.com/en-us/visualstudio/msbuild/how-to-use-project-sdk) from your project file (`.csproj` / `.vbproj`):
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="15.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Sdk Name="DasMulli.AssemblyInfoGeneration.Sdk" Version="1.0.0-beta001" />
+…
+```
---
+++ src/DasMulli.AssemblyInfoGeneration.Sdk/GetAssemblyVersion.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace DasMulli.AssemblyInfoGeneration.Sdk
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Determines the assembly version to use for a given semantic version.
+    /// </summary>
+    public class GetAssemblyVersion : ContextAwareTask
+    {
+        /// <summary>
+        /// The nuget version from which to get an assembly version portion.
+        /// </summary>
+        [Required]
+        public string NuGetVersion { get; set; }
+
+        /// <summary>
+        /// The assembly version (major.minor.patch.revision) portion of the nuget version.
+        /// </summary>
+        [Output]
+        public string AssemblyVersion { get; set; }
+
+        protected override bool ExecuteInner()
+        {
+            if (!NuGet.Versioning.NuGetVersion.TryParse(NuGetVersion, out var nugetVersion))
+            {
+                Log.LogError(
+                    subcategory: default,
+                    errorCode: "NETSDK1018",
+                    helpKeyword: default,
+                    file: default,
+                    lineNumber: default,
+                    columnNumber: default,
+                    endLineNumber: default,
+                    endColumnNumber: default,
+                    message: $"Invalid NuGet version string: '{NuGetVersion}'");
+                return false;
+            }
+
+            AssemblyVersion = nugetVersion.Version.ToString();
+            return true;
+        }
+    }
+}
